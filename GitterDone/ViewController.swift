9fe052ba71f172dca9e92b33647ef67c62c23d67
John Hawley <jehawley@gmail.com>
2018-09-02 19:42:24 -0700

    First Commit! Navigation & Task Adder Included

--- GitterDone/ViewController.swift
+++ GitterDone/ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var taskAdd: UITextField!
+    @IBOutlet var taskField: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func taskAdd(_ sender: Any) {
+        if let text = taskAdd.text {
+            if text == "" {
+                return
+            }
+            taskField.text.append("\(text)\n")
+            taskAdd.text = ""
+            taskAdd.resignFirstResponder()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
